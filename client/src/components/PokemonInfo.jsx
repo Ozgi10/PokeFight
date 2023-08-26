7bbfa834c5519f1d729b194f498a661017f010d4
Ozgi10 <129106787+Ozgi10@users.noreply.github.com>
2023-08-26 13:23:50 +0200

    add: deploy workflow

--- client/src/components/PokemonInfo.jsx
+++ client/src/components/PokemonInfo.jsx
@@ -1,23 +1,3 @@
-/*import useFetch from "../hooks/useFetchAll";
-import { useParams } from "react-router-dom";
-
-export default function PokemonInfo() {
-  const { id, info } = useParams();
-  const pokemonDetail = useFetch(id);
-
-  if (!pokemonDetail || !pokemonDetail[info]) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>{info} Info</h1>
-      <p>{pokemonDetail[info]}</p>
-    </div>
-  );
-}
-*/
-
 import useFetchInfo from "../hooks/useFetchInfo";
 import { useParams } from "react-router-dom";
 
